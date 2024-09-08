41f1a1ef72c07e980e7bd63fcc9640fb6e8bae7d
Gavin Johnson-Lynn <gav_jl@outlook.com>
2024-09-08 14:10:37 +0100

    Logout / Clear-Site-Data

---
+++ ASP.NetCore8/LogoutHeaders/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using LogoutHeaders.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LogoutHeaders.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Response.Cookies.Append("session", "123456789");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASP.NetCore8/LogoutHeaders/Controllers/LogoutController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LogoutHeaders.Controllers
+{
+    public class LogoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            Response.Headers.Append("Clear-Site-Data", "\"*\"");
+
+            foreach (var cookie in Request.Cookies.Keys)
+            {
+                Response.Cookies.Delete(cookie);
+            }
+
+            return View();
+        }
+    }
+}
